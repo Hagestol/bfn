29e00237305a291acbb8ca846e7e51b2e364f2ed
Morten Hindenes <morten.hindenes@outlook.com>
2023-02-01 14:03:40 +0100

    update mobile

--- src/pages/OmOss.tsx
+++ src/pages/OmOss.tsx
@@ -11,34 +11,12 @@ export default function HomePage() {
 						/>
 						<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">BFN</span>
 					</a>
-					<button
-						data-collapse-toggle="navbar-default"
-						type="button"
-						class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-						aria-controls="navbar-default"
-						aria-expanded="false"
-					>
-						<span class="sr-only">Open main menu</span>
-						<svg
-							class="w-6 h-6"
-							aria-hidden="true"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-								clip-rule="evenodd"
-							></path>
-						</svg>
-					</button>
-					<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+					<div class=" w-full md:block md:w-auto" id="navbar-default">
 						<ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 							<li>
 								<a
 									href="/"
-									class="block py-2 pl-3 pr-4 text-gray-700  bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-gray-400 hover:text-white"
+									class="block py-2 pl-3 pr-4 text-gray-700  rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-gray-400 hover:text-white"
 									aria-current="page"
 								>
 									Hjem
@@ -66,18 +44,18 @@ export default function HomePage() {
 				</div>
 			</nav>
 			<h1 class="text-3xl mt-12 sm:(mt-28 text-6xl)">BFN</h1>
-			<p class="text-3xl mt-6">Det beste League of Legends laget!</p>
-			<div class="mt-10 min-h-[100px] grid grid-cols-1 gap-2 content-center place-items-center">
+			<p class="text-2xl md:(text-3xl mt-6)">Det beste League of Legends laget!</p>
+			<div class="md:(mt-10 min-h-[100px] grid grid-cols-1 gap-2 content-center place-items-center)">
 				<a
 					href="#"
-					class="flex-col items-center justify-between block min-w-[800px] p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+					class="md:(flex-col items-center justify-between block min-w-[800px] p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700)"
 				>
 					<div class="">
 						<div class="w-full text-lg font-semibold">Telialigaen Rekrutt: League of Legends - våren 2023</div> <br />
 						<div class="flex items-center">
 							<div class="w-1/3 text-red-500">Taper</div>
 							<img
-								class="rounded-full "
+								class="rounded-full"
 								src="https://i.bo3.no/image/325696/Carolina-Panthers-logo-cover-958x575.png?c=1&h=42&w=42"
 								alt=""
 							/>
--- src/pages/medlemmer.tsx
+++ src/pages/medlemmer.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
 	return (
-		<div class="bg-gray-800 text-center  m-auto h-screen text-white">
+		<div class="bg-gray-800 text-center  m-auto min-h-screen text-white">
 			<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
 				<div class="container flex flex-wrap items-center justify-between mx-auto">
 					<a href="/" class="flex items-center">
@@ -11,34 +11,12 @@ export default function HomePage() {
 						/>
 						<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">BFN</span>
 					</a>
-					<button
-						data-collapse-toggle="navbar-default"
-						type="button"
-						class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-						aria-controls="navbar-default"
-						aria-expanded="false"
-					>
-						<span class="sr-only">Open main menu</span>
-						<svg
-							class="w-6 h-6"
-							aria-hidden="true"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-								clip-rule="evenodd"
-							></path>
-						</svg>
-					</button>
-					<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+					<div class=" w-full md:block md:w-auto" id="navbar-default">
 						<ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 							<li>
 								<a
 									href="/"
-									class="block py-2 pl-3 pr-4 text-gray-700 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-gray-400 hover:text-white"
+									class="block py-2 pl-3 pr-4 text-gray-700  rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-gray-400 hover:text-white"
 									aria-current="page"
 								>
 									Hjem
@@ -47,7 +25,7 @@ export default function HomePage() {
 							<li>
 								<a
 									href="OmOss"
-									class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+									class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 								>
 									Om oss
 								</a>
@@ -55,7 +33,7 @@ export default function HomePage() {
 							<li>
 								<a
 									href="medlemmer"
-									class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+									class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 								>
 									Medlemmer
 								</a>
@@ -67,8 +45,8 @@ export default function HomePage() {
 			</nav>
 
 			<h1 class="text-3xl mt-12 sm:(mt-28 text-6xl)">BFN</h1>
-			<p class="text-3xl mt-6">Medlemmer av BFN!</p>
-			<div class="mt-9 mr-5 ml-5 h-56 grid grid-cols-5 gap-5 content-center">
+			<p class="text-3xl mt-6 mb-6">Medlemmer av BFN!</p>
+			<div class="grid gap-4 max-w-[350px] mx-auto md:(grid-rows-1 grid-cols-5 max-w-full m-6)">
 				<a
 					href="#"
 					class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
